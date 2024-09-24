8ffea8f61ef7d939a2bc6aa7dca41cab983c81e6
alexvj34 <thealexvj@mail.ru>
2024-09-24 19:29:18 +0300

    Выполнил 8 домашнее задание

---
+++ src/main/java/ru/otus/java/basic/homework8/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homework8;
+
+public class AppArrayDataException extends IllegalArgumentException {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
